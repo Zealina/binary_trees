6c1bd768b002528d4a536f8ebad0cd0e2cfb58b0
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-08-02 12:06:23 +0000

    Count Non-Leaf Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,8 +12,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 	l_nodes = binary_tree_nodes(tree->left);
 	r_nodes = binary_tree_nodes(tree->right);
-
-	if (
+	return (1 + l_nodes + r_nodes);
+}
