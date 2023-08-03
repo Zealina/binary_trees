de00e77497a4621017ccfc07eb9a188e2cd0e48a
Hamilton Sebastine <hamilsebastine@gmail.com>
2023-08-03 07:54:06 +0000

    Return Sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Returns the sibling of a node in a binary tree
+ * @node: Pointer to the node
+ *
+ * Return: The pointer to the sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *trav;
+
+	trav = node->parent;
+	if (node == NULL || trav == NULL)
+		return (NULL);
+	if (trav->left == NULL || trav->right == NULL)
+		return (NULL);
+	if (trav->left == node)
+		return (trav->right);
+	return (trav->left);
+}
